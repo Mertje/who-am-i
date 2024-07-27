17e76dc946ddaaa4dcc855c19392e2d398ef2ea0
Mert G <mert34gunes@gmail.com>
2024-07-27 13:03:44 +0200

    Cleaning project, adding tailwind

---
+++ src/pages/home/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="bg-red-300">
+      <div className="grid justify-center">
+        Work in progress
+      </div>
+    </div>
+  )
+}
+
+export default App
